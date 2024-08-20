38df10299660be5d2a33fe54191826179da26c33
SunixPN <paxa14092005@gmail.com>
2024-08-20 13:03:56 +0300

    refs #master guards

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,29 +1,34 @@
-import { Body, Controller, Get, Post, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { ITaskCreate } from 'src/entities/task.model';
 import { User } from 'src/entities/user.model';
 import { Response } from 'express';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post("/create")
+  @UseGuards(AuthGuard)
   createTask(@Body() task: ITaskCreate) {
     return this.taskService.createTask(task)
   }
 
   @Get("/all-tasks-with-user")
+  @UseGuards(AuthGuard)
   allTasksWithUser(@Req() { user }) {
     return this.taskService.getAllTasksWithUser(user as User)
   }
 
   @Post("/start-task/:id")
+  @UseGuards(AuthGuard)
   startTask(@Req() { user }, @Query("id") id: string) {
     return this.taskService.startTask(user, +id)
   }
 
   @Post("/goToLink/:link/:id")
+  @UseGuards(AuthGuard)
   goToLink(
     @Req() { user }, 
     @Query("link") link: string,
@@ -34,6 +39,7 @@ export class TaskController {
   }
 
   @Post("/claim-task-coins/:id")
+  @UseGuards(AuthGuard)
   claimCoins(@Req() { user },  @Query("id") id: string) {
     return this.taskService.claimTaskCoins(user, +id)
   }
