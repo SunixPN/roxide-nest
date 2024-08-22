d8a2d445527daf48631efbf68d2892b7b5e100d2
SunixPN <paxa14092005@gmail.com>
2024-08-22 12:26:11 +0300

    refs #master fix

--- src/modules/revenues/revenues.module.ts
+++ src/modules/revenues/revenues.module.ts
@@ -3,12 +3,14 @@ import { RevenuesService } from './revenues.service';
 import { RevenuesController } from './revenues.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Revenues } from 'src/entities/revenues.model';
+import { UserModule } from '../user/user.module';
 
 @Module({
   controllers: [RevenuesController],
   providers: [RevenuesService],
   imports: [
-    SequelizeModule.forFeature([Revenues])
+    SequelizeModule.forFeature([Revenues]),
+    UserModule
   ]
 })
 export class RevenuesModule {}
